490e5eb02a4787b823fc0dd1ce8d7c9eea66a873
guillesanbri <guillesanbri@hotmail.com>
2022-03-04 19:04:03 +0100

    add wandb sweep support

--- Scripts/decision_tree_pipeline.py
+++ Scripts/decision_tree_pipeline.py
@@ -10,16 +10,23 @@ from metrics import compute_metrics
 
 if __name__ == "__main__":
     # Hyperparameter config
+    model = GradientBoostingRegressor
     hyperparameters_default = {
-        "model": "Gradient boosting",
-        "freq": "day",
-        "ID_sample": 10,
+        "model": model,
+        "learning_rate": 0.1,
+        "n_estimators": 100,
+        "max_depth": 3,
+        "freq": "hour",
+        "ID_sample": 100,
         "Time encoding": "RBF",
+        "weekday_rbf": True,
+        "monthly_rbf": True,
+        "hourly_rbf": True,
         "Holidays": False,
     }
 
     # Init wandb
-    wandb.init(project="UniversityHack", config=hyperparameters_default,)  # mode="disabled"
+    wandb.init(project="UniversityHack", config=hyperparameters_default)  # mode="disabled"
     config = wandb.config
 
     # Read data
@@ -35,30 +42,43 @@ if __name__ == "__main__":
         temp = temp.groupby(pd.Grouper(freq='D')).sum()
     temp["TARGET"] = temp["DELTA"]
     temp["YESTERDAY_DELTA"] = temp["DELTA"].shift(1)
-    print(temp.columns)
     # Time encoding
-    temp["day"] = temp.index.dayofyear
-    monthly_rbf = RepeatingBasisFunction(n_periods=12, column="day", input_range=(1, 365))
-    monthly_rbf.fit(temp)
-    monthly_rbf_results = monthly_rbf.transform(temp)
     monthly_rbf_cols = []
-    for i, col in enumerate(monthly_rbf_results.T):
-        col_name = f"month_rbf_{i+1}"
-        temp[col_name] = col
-        monthly_rbf_cols.append(col_name)
-    temp["weekday"] = temp.index.weekday
-    weekday_rbf = RepeatingBasisFunction(n_periods=7, column="weekday", input_range=(0, 6))
-    weekday_rbf.fit(temp)
-    weekday_rbf_results = weekday_rbf.transform(temp)
+    if config.monthly_rbf:
+        temp["day"] = temp.index.dayofyear
+        monthly_rbf = RepeatingBasisFunction(n_periods=12, column="day", input_range=(1, 365))
+        monthly_rbf.fit(temp)
+        monthly_rbf_results = monthly_rbf.transform(temp)
+        for i, col in enumerate(monthly_rbf_results.T):
+            col_name = f"month_rbf_{i+1}"
+            temp[col_name] = col
+            monthly_rbf_cols.append(col_name)
+
     weekday_rbf_cols = []
-    for i, col in enumerate(weekday_rbf_results.T):
-        col_name = f"weekday_rbf_{i + 1}"
-        temp[col_name] = col
-        weekday_rbf_cols.append(col_name)
+    if config.weekday_rbf:
+        temp["weekday"] = temp.index.weekday
+        weekday_rbf = RepeatingBasisFunction(n_periods=7, column="weekday", input_range=(0, 6))
+        weekday_rbf.fit(temp)
+        weekday_rbf_results = weekday_rbf.transform(temp)
+        for i, col in enumerate(weekday_rbf_results.T):
+            col_name = f"weekday_rbf_{i + 1}"
+            temp[col_name] = col
+            weekday_rbf_cols.append(col_name)
+
+    hour_rbf_cols = []
+    if config.freq == "hour" and config.hourly_rbf:
+        temp["hour"] = temp.index.hour
+        hour_rbf = RepeatingBasisFunction(n_periods=24, column="hour", input_range=(0, 23))
+        hour_rbf.fit(temp)
+        hour_rbf_results = hour_rbf.transform(temp)
+        for i, col in enumerate(hour_rbf_results.T):
+            col_name = f"hour_rbf_{i + 1}"
+            temp[col_name] = col
+            hour_rbf_cols.append(col_name)
 
     # Extract target and input columns.
     # Exclude the row with NaN values (due to shift operation)
-    X = temp[["YESTERDAY_DELTA", *monthly_rbf_cols, *weekday_rbf_cols]].to_numpy()[1:]
+    X = temp[["YESTERDAY_DELTA", *monthly_rbf_cols, *weekday_rbf_cols, *hour_rbf_cols]].to_numpy()[1:]
     y = temp["TARGET"].to_numpy()[1:]
 
     print(f"X shape: {X.shape}, y shape: {y.shape}")
@@ -78,15 +98,21 @@ if __name__ == "__main__":
         # Fittable feature transformations
         pass
         # Training
-        regressor = GradientBoostingRegressor(random_state=0)
+        regressor = model(random_state=0, learning_rate=config.learning_rate, n_estimators=config.n_estimators, max_depth=config.max_depth)
         regressor.fit(X_train, y_train)
         # Prediction
         predictions = np.zeros_like(y_test)
-        for p in range(14):
+        n_iter = len(y_test)  # 14 if config.freq == "day" else 14*24
+        for p in range(n_iter):
             predictions[p] = regressor.predict(X_test[p].reshape(1, -1))
-            if (p+1) < 14:
+            if (p+1) < n_iter:
                 X_test[p+1][0] = predictions[p]  # Overwrite YESTERDAY_DELTA in next time step
+
         # Print RMSE of 14 days predictions
+        if config.freq == "hour":
+            y_test = np.sum(y_test.reshape(-1, 24), axis=1)
+            predictions = np.sum(predictions.reshape(-1, 24), axis=1)
+
         rmse_metrics = compute_metrics(predictions, y_test)
         wandb.log({"cv_split": split+1, **rmse_metrics})
         print(rmse_metrics)
