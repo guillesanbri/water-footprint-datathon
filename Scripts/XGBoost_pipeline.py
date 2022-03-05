12f55efb10263dcefd2ea1394ecb873dfa53a58c
guillesanbri <guillesanbri@hotmail.com>
2022-03-05 21:58:12 +0100

    add average over multiple ids

--- Scripts/XGBoost_pipeline.py
+++ Scripts/XGBoost_pipeline.py
@@ -9,6 +9,7 @@ from collections import Counter
 
 from time_encoding import encode_hour, encode_month, encode_weekday
 from holidays import add_holiday_column
+from id_selection import validation_ids_v1
 
 if __name__ == "__main__":
     # Hyperparameter config
@@ -30,7 +31,7 @@ if __name__ == "__main__":
         "lambda": 1,
         "tree_method": "auto",
 
-        "ID_sample": 100,  # TODO
+        "IDs_samples": validation_ids_v1,
         "n_lags": 1,
         "monthly_encoding": "RBF",
         "weekday_encoding": "RBF",
@@ -39,94 +40,104 @@ if __name__ == "__main__":
     }
 
     # Init wandb
-    wandb.init(project="UniversityHack", config=hyperparameters_default, mode="disabled")  # mode="disabled"
+    wandb.init(project="UniversityHack", config=hyperparameters_default,)  # mode="disabled"
     config = wandb.config
 
-    # Read data
-    assert config.freq in ["day", "hour"]
-    df = pd.read_csv(f"../Data/water_meters/{str(config.ID_sample).zfill(4)}.csv")
-    print(f"Data from meter no. {config.ID_sample} loaded correctly.")
+    # Iterate over multiple IDs
+    mean_IDs = Counter()
+    for id_ in tqdm.tqdm(config.IDs_samples):
 
-    # Feature independent preprocessing
-    df["SAMPLETIME"] = pd.to_datetime(df["SAMPLETIME"], utc=True)
-    temp = df.copy()
-    temp.index = temp.SAMPLETIME
-    if config.freq == "day":
-        temp = temp.groupby(pd.Grouper(freq='D')).sum()
-    temp["TARGET"] = temp["DELTA"]
+        # Read data
+        assert config.freq in ["day", "hour"]
+        df = pd.read_csv(f"../Data/water_meters/{str(id_).zfill(4)}.csv")
+        # print(f"Data from meter no. {id_} loaded correctly.")
 
-    # Add lagged deltas
-    lagged_cols = []
-    for i in range(config.n_lags):
-        lag_name = f"delta_lag_{i+1}"
-        lagged_cols.append(lag_name)
-        temp[lag_name] = temp["DELTA"].shift(i+1)
+        # Feature independent preprocessing
+        df["SAMPLETIME"] = pd.to_datetime(df["SAMPLETIME"], utc=True)
+        temp = df.copy()
+        temp.index = temp.SAMPLETIME
+        if config.freq == "day":
+            temp = temp.groupby(pd.Grouper(freq='D')).sum()
+        temp["TARGET"] = temp["DELTA"]
 
-    # Add holiday data
-    additional_cols = []
-    if config.holidays:
-        col_name = "holiday"
-        additional_cols.append(col_name)
-        temp = add_holiday_column(temp, col_name)
+        # Add lagged deltas
+        lagged_cols = []
+        for i in range(config.n_lags):
+            lag_name = f"delta_lag_{i+1}"
+            lagged_cols.append(lag_name)
+            temp[lag_name] = temp["DELTA"].shift(i+1)
 
-    # Time encoding
-    # # Month encoding
-    temp, month_encoding_cols = encode_month(temp, config.monthly_encoding)
-    # # Weekday encoding
-    temp, weekday_encoding_cols = encode_weekday(temp, config.weekday_encoding)
-    # # Hour encoding
-    temp, hour_encoding_cols = encode_hour(temp, config.hourly_encoding) if config.freq == "hour" else (temp, [])
+        # Add holiday data
+        additional_cols = []
+        if config.holidays:
+            col_name = "holiday"
+            additional_cols.append(col_name)
+            temp = add_holiday_column(temp, col_name)
 
-    # Extract target and input columns.
-    # Exclude rows with NaN values (due to shift operation)
-    X = temp[[*lagged_cols,
-              *additional_cols,
-              *month_encoding_cols, *weekday_encoding_cols, *hour_encoding_cols]].to_numpy()[config.n_lags:]
-    y = temp["TARGET"].to_numpy()[config.n_lags:]
+        # Time encoding
+        # # Month encoding
+        temp, month_encoding_cols = encode_month(temp, config.monthly_encoding)
+        # # Weekday encoding
+        temp, weekday_encoding_cols = encode_weekday(temp, config.weekday_encoding)
+        # # Hour encoding
+        temp, hour_encoding_cols = encode_hour(temp, config.hourly_encoding) if config.freq == "hour" else (temp, [])
 
-    print(f"X shape: {X.shape}, y shape: {y.shape}")
+        # Extract target and input columns.
+        # Exclude rows with NaN values (due to shift operation)
+        X = temp[[*lagged_cols,
+                  *additional_cols,
+                  *month_encoding_cols, *weekday_encoding_cols, *hour_encoding_cols]].to_numpy()[config.n_lags:]
+        y = temp["TARGET"].to_numpy()[config.n_lags:]
 
-    # Time series cross validation
-    # 13 splits of two weeks: first training uses the first 6 months of data.
-    # Last split trains with all but the two last weeks of data.
-    n_splits = 13
-    tscv = TimeCrossValidator(data_freq=config.freq, n_splits=n_splits, test_days=14)
-    mean_metrics = Counter()
-    for split, (train_index, test_index) in tqdm.tqdm(enumerate(tscv.split(X)), total=n_splits):
-        # cv_split_title = f"  CV Split no. {split + 1}  "
-        # print("="*len(cv_split_title))
-        # print(cv_split_title)
-        # print("="*len(cv_split_title))
+        # print(f"X shape: {X.shape}, y shape: {y.shape}")
 
-        # Separate train and test
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        # Fittable feature transformations
-        pass
-        # Training
-        regressor = model(random_state=0, learning_rate=config.learning_rate, n_estimators=config.n_estimators, max_depth=config.max_depth)
-        regressor.fit(X_train, y_train)
-        # Prediction
-        predictions = np.zeros_like(y_test)
-        n_iter = len(y_test)  # 14 if config.freq == "day" else 14*24
-        for p in range(n_iter):
-            predictions[p] = regressor.predict(X_test[p].reshape(1, -1))
-            if (p+1) < n_iter:
-                for i in range(config.n_lags):
-                    if i == 0:
-                        X_test[p+1][i] = predictions[p]  # Overwrite delta-1 in next time step
-                    else:
-                        X_test[p+1][i] = X_test[p][i-1]
+        # Time series cross validation
+        # 13 splits of two weeks: first training uses the first 6 months of data.
+        # Last split trains with all but the two last weeks of data.
+        n_splits = 13
+        tscv = TimeCrossValidator(data_freq=config.freq, n_splits=n_splits, test_days=14)
+        mean_metrics = Counter()
+        for split, (train_index, test_index) in enumerate(tscv.split(X)):
+            # cv_split_title = f"  CV Split no. {split + 1}  "
+            # print("="*len(cv_split_title))
+            # print(cv_split_title)
+            # print("="*len(cv_split_title))
 
-        # Print RMSE of 14 days predictions
-        if config.freq == "hour":
-            y_test = np.sum(y_test.reshape(-1, 24), axis=1)
-            predictions = np.sum(predictions.reshape(-1, 24), axis=1)
+            # Separate train and test
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            # Fittable feature transformations
+            pass
+            # Training
+            regressor = model(random_state=0, nthread=2,
+                              learning_rate=config.learning_rate, n_estimators=config.n_estimators, max_depth=config.max_depth)
+            regressor.fit(X_train, y_train)
+            # Prediction
+            predictions = np.zeros_like(y_test)
+            n_iter = len(y_test)  # 14 if config.freq == "day" else 14*24
+            for p in range(n_iter):
+                predictions[p] = regressor.predict(X_test[p].reshape(1, -1))
+                if (p+1) < n_iter:
+                    for i in range(config.n_lags):
+                        if i == 0:
+                            X_test[p+1][i] = predictions[p]  # Overwrite delta-1 in next time step
+                        else:
+                            X_test[p+1][i] = X_test[p][i-1]
 
-        rmse_metrics = compute_metrics(predictions, y_test)
-        wandb.log({"cv_split": split+1, **rmse_metrics})
-        mean_metrics.update(rmse_metrics)
-    mean_metrics = {k: v/n_splits for k, v in mean_metrics.items()}
-    print(f"Mean of metrics along {n_splits} splits:")
-    print(mean_metrics)
-    wandb.log({"cv_split": "mean", **mean_metrics})
+            # Print RMSE of 14 days predictions
+            if config.freq == "hour":
+                y_test = np.sum(y_test.reshape(-1, 24), axis=1)
+                predictions = np.sum(predictions.reshape(-1, 24), axis=1)
+
+            rmse_metrics = compute_metrics(predictions, y_test)
+            # wandb.log({"cv_split": split+1, **rmse_metrics})
+            mean_metrics.update(rmse_metrics)
+        mean_metrics = {k: v/n_splits for k, v in mean_metrics.items()}
+        # print(f"Mean of metrics along {n_splits} splits:")
+        # print(mean_metrics)
+        wandb.log({"ID": id_, **mean_metrics})
+        mean_IDs.update(mean_metrics)
+    mean_IDs = {f"mean_IDs_{k}": v / len(config.IDs_samples) for k, v in mean_IDs.items()}
+    print(f"Mean of metrics along {len(config.IDs_samples)} IDs:")
+    print(mean_IDs)
+    wandb.log({**mean_IDs})
--- Scripts/sample_pipeline.py
+++ Scripts/sample_pipeline.py
@@ -5,66 +5,75 @@ import tqdm
 from cross_validation import TimeCrossValidator
 from metrics import compute_metrics
 from collections import Counter
+from id_selection import validation_ids_v1
 
 if __name__ == "__main__":
     # Hyperparameter config
     hyperparameters_default = {
         "model": "Yesterday delta naive baseline",
         "freq": "day",
-        "ID_sample": 100,
+        "IDs_samples": validation_ids_v1,
     }
 
     # Init wandb
     wandb.init(project="UniversityHack", config=hyperparameters_default, mode="disabled")
     config = wandb.config
 
-    # Read data
-    assert config.freq in ["day", "hour"]
-    df = pd.read_csv(f"../Data/water_meters/{str(config.ID_sample).zfill(4)}.csv")
-    print(f"Data from meter no. {config.ID_sample} loaded correctly.")
+    # Iterate over multiple IDs
+    mean_IDs = Counter()
+    for id_ in tqdm.tqdm(config.IDs_samples):
 
-    # Feature independent preprocessing
-    df["SAMPLETIME"] = pd.to_datetime(df["SAMPLETIME"], utc=True)
-    temp = df.copy()
-    temp.index = temp.SAMPLETIME
-    if config.freq == "day":
-        temp = temp.groupby(pd.Grouper(freq='D')).sum()
-    temp["TARGET"] = temp["DELTA"]
-    temp["YESTERDAY_DELTA"] = temp["DELTA"].shift(1)
-    temp["YESTERDAY_READING"] = temp["READING"].shift(1)
+        # Read data
+        assert config.freq in ["day", "hour"]
+        df = pd.read_csv(f"../Data/water_meters/{str(id_).zfill(4)}.csv")
+        # print(f"Data from meter no. {id_} loaded correctly.")
 
-    # Extract target and columns.
-    # Exclude the row with NaN values (due to shift operation)
-    X = temp[["YESTERDAY_READING", "YESTERDAY_DELTA"]].to_numpy()[1:]
-    y = temp["TARGET"].to_numpy()[1:]
+        # Feature independent preprocessing
+        df["SAMPLETIME"] = pd.to_datetime(df["SAMPLETIME"], utc=True)
+        temp = df.copy()
+        temp.index = temp.SAMPLETIME
+        if config.freq == "day":
+            temp = temp.groupby(pd.Grouper(freq='D')).sum()
+        temp["TARGET"] = temp["DELTA"]
+        temp["YESTERDAY_DELTA"] = temp["DELTA"].shift(1)
+        temp["YESTERDAY_READING"] = temp["READING"].shift(1)
 
-    print(f"X shape: {X.shape}, y shape: {y.shape}")
+        # Extract target and columns.
+        # Exclude the row with NaN values (due to shift operation)
+        X = temp[["YESTERDAY_READING", "YESTERDAY_DELTA"]].to_numpy()[1:]
+        y = temp["TARGET"].to_numpy()[1:]
 
-    # Time series cross validation
-    # 13 splits of two weeks: first training uses the first 6 months of data.
-    # Last split trains with all but the two last weeks of data.
-    n_splits = 13
-    tscv = TimeCrossValidator(data_freq=config.freq, n_splits=n_splits, test_days=14)
-    mean_metrics = Counter()
-    for split, (train_index, test_index) in tqdm.tqdm(enumerate(tscv.split(X)), total=n_splits):
-        # Separate train and test
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        # Fittable feature transformations
-        pass
-        # Training
-        pass  # Naive model
-        # Prediction
-        predictions = np.zeros_like(y_test)
-        for p in range(14):
-            predictions[p] = X_test[p][1]  # Naive baseline: Copy YESTERDAY_DELTA
-            if (p+1) < 14:
-                X_test[p+1][1] = predictions[p]  # Overwrite YESTERDAY_DELTA in next time step
-        # Print RMSE of 14 days predictions
-        rmse_metrics = compute_metrics(predictions, y_test)
-        wandb.log({"cv_split": split+1, **rmse_metrics})
-        mean_metrics.update(rmse_metrics)
-    mean_metrics = {k: v / n_splits for k, v in mean_metrics.items()}
-    print(f"Mean of metrics along {n_splits} splits:")
-    print(mean_metrics)
-    wandb.log({"cv_split": "mean", **mean_metrics})
+        # print(f"X shape: {X.shape}, y shape: {y.shape}")
+
+        # Time series cross validation
+        # 13 splits of two weeks: first training uses the first 6 months of data.
+        # Last split trains with all but the two last weeks of data.
+        n_splits = 13
+        tscv = TimeCrossValidator(data_freq=config.freq, n_splits=n_splits, test_days=14)
+        mean_metrics = Counter()
+        for split, (train_index, test_index) in enumerate(tscv.split(X)):
+            # Separate train and test
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            # Fittable feature transformations
+            pass
+            # Training
+            pass  # Naive model
+            # Prediction
+            predictions = np.zeros_like(y_test)
+            for p in range(14):
+                predictions[p] = X_test[p][1]  # Naive baseline: Copy YESTERDAY_DELTA
+                if (p + 1) < 14:
+                    X_test[p + 1][1] = predictions[p]  # Overwrite YESTERDAY_DELTA in next time step
+            # Print RMSE of 14 days predictions
+            rmse_metrics = compute_metrics(predictions, y_test)
+            mean_metrics.update(rmse_metrics)
+        mean_metrics = {k: v/n_splits for k, v in mean_metrics.items()}
+        # print(f"Mean of metrics along {n_splits} splits:")
+        # print(mean_metrics)
+        wandb.log({"ID": id_, **mean_metrics})
+        mean_IDs.update(mean_metrics)
+    mean_IDs = {f"mean_IDs_{k}": v / len(config.IDs_samples) for k, v in mean_IDs.items()}
+    print(f"Mean of metrics along {len(config.IDs_samples)} IDs:")
+    print(mean_IDs)
+    wandb.log({**mean_IDs})
