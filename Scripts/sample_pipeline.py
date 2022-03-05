e7c5df59debc94ba5a21f4ccc377d74a1e9f41e1
guillesanbri <guillesanbri@hotmail.com>
2022-03-05 17:36:22 +0100

    update baseline pipeline

--- Scripts/sample_pipeline.py
+++ Scripts/sample_pipeline.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import numpy as np
 import wandb
+import tqdm
 from cross_validation import TimeCrossValidator
 from metrics import compute_metrics
+from collections import Counter
 
 if __name__ == "__main__":
     # Hyperparameter config
     hyperparameters_default = {
         "model": "Yesterday delta naive baseline",
         "freq": "day",
-        "ID_sample": 10,
+        "ID_sample": 100,
     }
 
     # Init wandb
-    wandb.init(project="UniversityHack", config=hyperparameters_default)
+    wandb.init(project="UniversityHack", config=hyperparameters_default, mode="disabled")
     config = wandb.config
 
     # Read data
@@ -41,12 +43,10 @@ if __name__ == "__main__":
     # Time series cross validation
     # 13 splits of two weeks: first training uses the first 6 months of data.
     # Last split trains with all but the two last weeks of data.
-    tscv = TimeCrossValidator(data_freq=config.freq, n_splits=13, test_days=14)
-    for split, (train_index, test_index) in enumerate(tscv.split(X)):
-        cv_split_title = f"  CV Split no. {split + 1}  "
-        print("="*len(cv_split_title))
-        print(cv_split_title)
-        print("="*len(cv_split_title))
+    n_splits = 13
+    tscv = TimeCrossValidator(data_freq=config.freq, n_splits=n_splits, test_days=14)
+    mean_metrics = Counter()
+    for split, (train_index, test_index) in tqdm.tqdm(enumerate(tscv.split(X)), total=n_splits):
         # Separate train and test
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
@@ -63,4 +63,8 @@ if __name__ == "__main__":
         # Print RMSE of 14 days predictions
         rmse_metrics = compute_metrics(predictions, y_test)
         wandb.log({"cv_split": split+1, **rmse_metrics})
-        print(rmse_metrics)
+        mean_metrics.update(rmse_metrics)
+    mean_metrics = {k: v / n_splits for k, v in mean_metrics.items()}
+    print(f"Mean of metrics along {n_splits} splits:")
+    print(mean_metrics)
+    wandb.log({"cv_split": "mean", **mean_metrics})
